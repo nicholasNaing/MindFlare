f44ea7ff7d0cf6b09d4438e8240b21ca5aaf04d3
Nicholas-L-Naing <htutoonaing221b@gmail.com>
2023-12-02 20:00:18 +0630

    change the config style

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base:"/MindFlare/"
+export default defineConfig(({command})=>{
+  const config = {
+    plugins: [react()],
+    base:"/"
+  }
+  if(command !== "serve"){
+    config.base = "/MindFlare/"
+  }
+  return config
 })
