2354dddd180c4be81ba7e79ba7d4496e2d5f6dd4
Nicholas-L-Naing <htutoonaing221b@gmail.com>
2023-12-03 00:07:44 +0630

    fixing error on react-router-dom

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+console.log("object");
 export default defineConfig(({command})=>{
   const config = {
     plugins: [react()],
     base:"/"
+    
   }
   if(command !== "serve"){
+    console.log("running")
     config.base = "/MindFlare/"
   }
   return config
