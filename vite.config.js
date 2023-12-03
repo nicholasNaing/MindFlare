faa6bd05854e05f21f5ba7b3fd5fb2c17ac500f1
Nicholas-L-Naing <htutoonaing221b@gmail.com>
2023-12-03 12:23:32 +0630

    fixing the error on vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
-console.log("object");
-export default defineConfig(({command})=>{
-  const config = {
-    plugins: [react()],
-    base:"/"
-    
-  }
-  if(command !== "serve"){
-    console.log("running")
-    config.base = "/MindFlare/"
-  }
-  return config
-})
+const isServe = process.env.MODE === 'serve';
+
+const config = defineConfig({
+  plugins: [react()],
+  base: isServe ? '/' : '/MindFlare/',
+});
+
+export default config;
